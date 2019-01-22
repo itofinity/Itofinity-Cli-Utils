939ac71bef8a7d3993be423cc357ea10cec9d17e
Mike Minns <mminns@itofinity.co.uk>
2019-01-22 16:20:51 +0000

    Initial commit

---
+++ src/Itofinity.Cli.Utils/Options/Global/Token.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+
+namespace Itofinity.Refit.Cli.Utils.Options.Global
+{
+    public class Token : AbstractOptionDefinition
+    {
+        public override string Template => "-t|--token";
+
+        public override string Description => "An authentication Token";
+
+        public override CommandOptionType OptionType => CommandOptionType.SingleValue;
+    }
+}
---
+++ src/Itofinity.Cli.Utils/Options/Global/Url.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+
+namespace Itofinity.Refit.Cli.Utils.Options.Global
+{
+    public class Url : AbstractOptionDefinition
+    {
+        public override string Template => "-u|--url";
+
+        public override string Description => "Root API Url";
+
+        public override CommandOptionType OptionType => CommandOptionType.SingleValue;
+    }
+}
